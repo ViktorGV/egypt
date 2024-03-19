5c758d25caf53c7c8bb68068629658688036813f
Viktor Varbanov <varbanov@uber.com>
2024-03-19 16:37:13 +0200

    Lost 30 min in this

--- src/main/java/com/uber/egypt/signature/InvalidDocumentFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.uber.egypt.signature;
-
-public class InvalidDocumentFormatException extends RuntimeException {
-
-    public InvalidDocumentFormatException(Exception e) {
-        super(e);
-    }
-}
--- src/src/test/resources/certs/delete.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm ca-cert.pem ca-key.pem signer-cert.pem signer-key.pem signer-req.csr signer-key-store.p12
