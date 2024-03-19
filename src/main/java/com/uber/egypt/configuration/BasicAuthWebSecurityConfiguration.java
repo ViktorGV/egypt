f8d64c02f715aea759161257b435bfd2cb9f68ec
Viktor Varbanov <varbanov@uber.com>
2024-03-19 16:55:58 +0200

    I am an idiot 5

--- src/main/java/com/uber/egypt/configuration/BasicAuthWebSecurityConfiguration.java
+++ src/main/java/com/uber/egypt/configuration/BasicAuthWebSecurityConfiguration.java
@@ -10,8 +10,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +31,7 @@ public class BasicAuthWebSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.
                 authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable).
                 httpBasic(Customizer.withDefaults()).
@@ -40,12 +43,11 @@ public class BasicAuthWebSecurityConfiguration {
         var userName = configurationReader.getUserName();
         var password = configurationReader.getPassword();
 
-        var passwordEncoder = new BCryptPasswordEncoder();
-        var encryptedPassword = passwordEncoder.encode(password);
+        var encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
         var user = User.builder()
                 .username(userName)
-                .password(encryptedPassword)
+                .password(encoder.encode(password))
                 .roles("USER")
                 .build();
 
